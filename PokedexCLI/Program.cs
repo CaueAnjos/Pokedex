e01ca9ce7753733260835bea9b6eacfc34f6d8b1
CaueAnjos <kawidosanjos@outlook.com>
2025-06-26 12:22:13 -0300

    refactor(rootCommand): add function to SetAction instead of a lambda

--- PokedexCLI/Program.cs
+++ PokedexCLI/Program.cs
@@ -5,41 +5,41 @@ using PokedexCLI.Models;
 
 internal class Program
 {
+    public static Argument<string> nameArgument = new("name");
+    public static Option<bool> jsonOption = new("--json");
+
     private static async Task<int> Main(string[] args)
     {
         RootCommand rootCommand = new("A CLI for Pokedex");
 
-        Argument<string> nameArgument = new("Pokemon name");
-        Option<bool> jsonOption = new("--json");
-
         rootCommand.Options.Add(jsonOption);
         rootCommand.Arguments.Add(nameArgument);
 
-        PokeApiFacade api = new();
-        rootCommand.SetAction(
-            async (parseResult) =>
-            {
-                bool? isJson = parseResult.GetValue(jsonOption);
-                if (isJson is not null && isJson.Value)
-                {
-                    string json = await api.GetJsonPokemonInfoAsync(
-                        parseResult.GetValue(nameArgument) ?? ""
-                    );
-                    Console.WriteLine(json);
-                }
-                else
-                {
-                    Pokemon pokemon = await api.GetPokemonInfoAsync(
-                        parseResult.GetValue(nameArgument) ?? ""
-                    );
-                    pokemon.PrintBeautiful();
-                }
-            }
-        );
+        rootCommand.SetAction(RootCommandAction);
 
         rootCommand.Subcommands.Add(new TamagotchiCommand());
 
         ParseResult parseResult = rootCommand.Parse(args);
         return await parseResult.InvokeAsync();
+    }
+
+    public static async Task RootCommandAction(ParseResult parseResult)
+    {
+        PokeApiFacade api = new();
+        bool? isJson = parseResult.GetValue(jsonOption);
+        if (isJson is not null && isJson.Value)
+        {
+            string json = await api.GetJsonPokemonInfoAsync(
+                parseResult.GetValue(nameArgument) ?? ""
+            );
+            Console.WriteLine(json);
+        }
+        else
+        {
+            Pokemon pokemon = await api.GetPokemonInfoAsync(
+                parseResult.GetValue(nameArgument) ?? ""
+            );
+            pokemon.PrintBeautiful();
+        }
     }
 }
